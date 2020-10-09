4f3275fa000d085511410caf3bc6b9dd7ae257f0
Miguel Ángel García Calderón <tonsquemike@gmail.com>
2020-10-09 15:56:57 -0500

    ejemplo1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { saveAsExcel } from './XLSX/XLS'
+import logoINE from "./Assets/Rep_INELogo.png";
+import logoSistema from './Assets/Juntas_01.png'
+import {columnasSeleccionadas, anchuraXLS, headerNames, tableColumns} from './RepCuaderno'
+//import {columnasSeleccionadas, anchuraXLS, headerNames, tableColumns} from './RepConcSesionesCons'
 function App() {
+  let nombreReporte = 'TEST OLIVER'
+  let headerData = [
+    'INE',
+    'EDOMEX - TOLUCA',
+    'Reporte ',
+    'Fecha reporte ',
+  ]
+
+  saveAsExcel('TEST REPORTE', logoINE, [],
+      columnasSeleccionadas, headerNames, logoSistema, headerData,
+      anchuraXLS, undefined,
+      undefined, nombreReporte, false, tableColumns)
   return (
     <div className="App">
       <header className="App-header">
